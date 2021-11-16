a0caafad3aecb0754570e93dad2bd56ca2572f75
ivan santana <iam.ivansantana@gmail.com>
2021-11-15 22:06:27 -0600

    routes done

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,39 +1,39 @@
-import {
-	// useContext,
-	// useEffect,
-	useState,
-} from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
-// import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
 
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
 import { AuthRouter } from './AuthRouter.js';
 import PinterestScreen from '../components/pinterestScreen/PinterestScreen';
-// import AuthContext from '../context/authContext/AuthContext';
+import AuthContext from '../context/authContext/AuthContext';
 
 export const AppRouter = () => {
-	// const { login } = useContext(AuthContext);
-	// const [checking, setChecking] = useState(true);
+	const { login } = useContext(AuthContext);
+	const [checking, setChecking] = useState(true);
 
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	// useEffect(() => {
-	// 	const auth = getAuth();
-	// 	onAuthStateChanged(auth, (user) => {
-	// 		if (user?.uid) {
-	// 			login(user.uid, user.displayName);
-	// 			setIsLoggedIn(true);
-	// 		} else {
-	// 			setIsLoggedIn(false);
-	// 		}
-	// 		setChecking(false);
-	// 	});
-	// }, []);
+	useEffect(() => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user?.uid) {
+				console.log(user?.uid);
+				console.log(user.uid, user.displayName);
+				login(user.uid, user.displayName);
+				setIsLoggedIn(true);
+			} else {
+				console.log('Error aquii');
+				setIsLoggedIn(false);
+			}
+			setChecking(false);
+		});
+		// eslint-disable-next-line
+	}, []);
 
-	// if (checking) {
-	// 	return <h1>Espere...</h1>;
-	// }
+	if (checking) {
+		return <h1>Espere...</h1>;
+	}
 
 	return (
 		<Router>
