71abecc0b2bda8a6cfbd33e7d300c177eb41a2d8
ivan santana <iam.ivansantana@gmail.com>
2021-11-21 23:11:42 -0600

    add others types on children interface

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,12 +1,15 @@
-import { ReactChild, ReactChildren, useContext } from 'react';
+import { ReactChild, ReactChildren, ReactElement, useContext } from 'react';
 import { Navigate } from 'react-router-dom';
+import { JsxElement } from 'typescript';
 import AuthContext from '../context/authContext/AuthContext';
 
 interface iChildren {
-	children: ReactChild | ReactChildren;
+	children: ReactChild | ReactChildren | JSX.Element[] | JSX.Element;
 }
 
-export const PrivateRoute = ({ children }: iChildren) => {
+export const PrivateRoute = ({
+	children,
+}: iChildren): ReactElement<JsxElement> => {
 	//Senecesita un state que indique si el usuario está autenticado o no
 	const { authState } = useContext(AuthContext);
 
